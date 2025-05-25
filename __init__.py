402031ad5e615aab9a3b9025542ddf72a933d12e
djryner <164276061+djryner@users.noreply.github.com>
2025-05-25 13:58:28 -0500

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,28 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from .const import DOMAIN
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    # We only support config entries, no YAML config
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up Donetick from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = {
+        "api_url": entry.data["api_url"],
+        "api_token": entry.data["api_token"],
+        "name": entry.data.get("name"),
+    }
+
+    # Forward setup to sensor platform
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    unload_ok = await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
